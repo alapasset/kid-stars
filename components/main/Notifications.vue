51e515b922b8afa3b030cf6a76161c31e7e2cb63
Alexis Lapasset <alexis.lapasset@gmail.com>
2024-02-28 23:13:31 +0100

    chore: add notifications

---
+++ components/main/Notifications.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const { notifications } = useNotifications();
+</script>
+
+<template>
+  <aside class="fixed bottom-0 md:bottom-2 inset-x-0 md:left-auto md:right-2 z-100">
+    <TransitionGroup
+      name="snackbar"
+      class="flex flex-col md:gap-3"
+      tag="div"
+      enter-active-class="transition-all ease-in-out duration-500"
+      leave-active-class="transition-all ease-in-out duration-500"
+      move-class="transition-all ease-in-out duration-500"
+      enter-from-class="opacity-0 translate-y-0"
+      leave-to-class="opacity-0 translate-y-0"
+    >
+      <MainSnackbar
+        v-for="notification in notifications"
+        :key="notification.id"
+        :notification="notification"
+      />
+    </TransitionGroup>
+  </aside>
+</template>
---
+++ components/main/Snackbar.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { NotificationType, type Notification } from '~/types/notifications';
+
+const props = defineProps<{
+  notification: Notification
+}>();
+
+const { removeNotification } = useNotifications();
+function removeCurrentNotification() {
+  removeNotification(props.notification.id);
+}
+setTimeout(removeCurrentNotification, 7000);
+</script>
+
+<template>
+  <div
+    class="flex items-center gap-3 p-2 md:p-4 md:rounded-lg shadow-md w-full text-white"
+    :class="notification?.type === NotificationType.Error ? `bg-red-500` : `bg-green-400`"
+  >
+    <span class="flex-1">{{ notification?.message }}</span>
+    <VBtn
+      icon="mdi-close"
+      variant="text"
+      @click="removeCurrentNotification"
+    />
+  </div>
+</template>
